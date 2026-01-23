f8d044e672608f31bb84276de6b2e4fcb89ea72e
soltsega <tsegasolomon538@gmail.com>
2026-01-23 19:31:41 +0300

    Testing script for hte installation of the dependencies

---
+++ scripts/test_install.py
@@ -0,0 +1,13 @@
+import yfinance as yf
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from statsmodels.tsa.stattools import adfuller
+import pmdarima as pm
+import sklearn
+import scipy
+import pytest
+import dotenv
+
+print("Installation successful!")
